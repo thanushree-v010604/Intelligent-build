55bda0cdbbb6521ae49cac492714062f7edf9973
Thanu Gowda <thanushreev010604@gmail.com>
2026-02-10 14:09:54 +0530

    Add frontend UI for demo

--- frontend/script.js
+++ frontend/script.js
@@ -1,21 +1,3 @@
-function runPipeline() {
-  const output = document.getElementById("output");
-
-  output.innerHTML = "🔄 Starting Build Pipeline...<br>";
-
-  setTimeout(() => {
-    output.innerHTML += "✅ Code fetched from repository<br>";
-  }, 1000);
-
-  setTimeout(() => {
-    output.innerHTML += "✅ Dependencies installed<br>";
-  }, 2000);
-
-  setTimeout(() => {
-    output.innerHTML += "✅ Build completed successfully<br>";
-  }, 3000);
-
-  setTimeout(() => {
-    output.innerHTML += "<br>🎉 <b>Build Status: SUCCESS</b>";
-  }, 4000);
+function showMessage() {
+    alert("Build and Release Pipeline Triggered!");
 }
